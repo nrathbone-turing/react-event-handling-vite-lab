b48a3d77d8c9f96051d05103bcfd34945fb9f70c
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 11:33:16 -0600

    Create event handler function for PasswordInput

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,17 @@
-// Code PasswordInput Component Here
+import React from "react";
+
 
 function PasswordInput (){
-    return(
-        <></>
-    )
+    const handleChange = () => {
+        console.log("Entering password...");
+    };
+
+    return (
+        <input
+            type="password"
+            onChange={handleChange}
+        />
+  );
 }
 
 export default PasswordInput;
