2141e5430bd8154431129e57328e3c1589f84b8b
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 11:38:27 -0600

    Create event handler functions for SubmitButton that tracks when mouse movement enters or leaves the button

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,16 @@
-// Code SubmitButton Component Here
+import React from "react";
 
 function SubmitButton (){
+    const handleEnter = () => {
+        console.log("Mouse Entering")
+    }
+
+    const handleLeave = () => {
+        console.log("Mouse Exiting")
+    }
+
     return(
-        <></>
+        <button onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Submit Password</button>
     )
 }
 
